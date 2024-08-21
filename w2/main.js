9019d78b255ef2947a7c87ebb02b6c294eb3b822
Roberto Juandedios <rjuandedios14@gmail.com>
2024-08-21 13:07:09 -0700

    added comments to js file

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,31 @@
-console.log("hello")
+// How to Calculate Your Carbon Footprint
+
+// Method 1 Calculating Your Carbon Footprint Points
+// 1 Count the members of your household.
+
+// 2 Consider the size of your home
+
+// 3 Evaluate your food choices.
+
+// 4 Examine your water consumption.
+
+// 5 Determine how many household purchases you make each year.
+
+// 6 Consider how much waste you produce.
+
+// 7 Identify the amount of waste that you recycle. 
+
+// 8 Tally up your annual transportation scores.
+
+// 9 Add up your points.
+
+// Method 2 Calculating the Impact of Your Car and Utilities
+// 1 Add 2 to 12 tons of CO2 emission if you own a car.
+
+// 2 Find your total fuel usage.
+
+// 3 Multiply your fuel gallons by a conversion factor. 
+
+// 4 Check your monthly utility bills. 
+
+// 5 Consider any offsets you have made.
