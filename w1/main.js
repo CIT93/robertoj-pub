d160a26f0a03bb072bd0ebc73d41421e619fea5e
Roberto Juandedios <rjuandedios14@gmail.com>
2024-08-26 17:22:57 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+console.log("hello from inside the main.js file")
+
+// let myVar;
+
+const myVar = "rj";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will 11 this one run?`)
+    } else {
+        console.log(`will 13 this one run?`)
+    }
+}
+
+runNow();
+runNow();
