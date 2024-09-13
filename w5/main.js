1577a1a86c9cd7be2c5a781021e03aa906cd76e0
Roberto Juandedios <rjuandedios14@gmail.com>
2024-09-12 22:44:26 -0700

    array of objects challenge

--- w5/main.js
+++ w5/main.js
@@ -50,10 +50,10 @@ function start(HouseHoldMembers, houseSize) {
     const houseSizePTS = determineHomeSizePts(houseSize);
     const total = houseHoldPTS + houseSizePTS;
     cfpData.push({
-        HouseM: HouseHoldMembers,
+        houseM: HouseHoldMembers,
         houseS: houseSize,
         houseMPTS: houseHoldPTS,
-        homeSPTS: houseSizePTS,
+        houseSPTS: houseSizePTS,
         cfpTotal: total
     });
 }
@@ -65,14 +65,14 @@ function displayOutput() {
         const output = document.getElementById("output");
         const newH2 = document.createElement("h2");
         newH2.textContent = `Cardon Footprint ${obj.cfpTotal}`;
-//         const newH3 = document.createElement("h3");
-//         newH3.textContent = 'Based on number in and size of home'
-//         const newP = document.createElement("p");
-//         newP.textContent = `This number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}),`;
-//         newP.textContent += ` and a ${arr[1]} size of home (score:${arr[2]}).`;
+        const newH3 = document.createElement("h3");
+        newH3.textContent = 'Based on number in and size of home'
+        const newP = document.createElement("p");
+        newP.textContent = `This number is based on the number of people in the house of ${obj.houseM} (score: ${obj.houseMPTS}),`;
+        newP.textContent += ` and a ${obj.houseS} size of home (score:${obj.houseSPTS}).`;
         output.appendChild(newH2);
-//         output.appendChild(newH3);
-//         output.appendChild(newP);
+        output.appendChild(newH3);
+        output.appendChild(newP);
     }
 }
 
