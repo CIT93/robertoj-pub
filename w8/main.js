2c4abef36907e33d2854eb359db456f869718c1d
Roberto Juandedios <rjuandedios14@gmail.com>
2024-10-03 23:58:46 -0700

    starting code ready

---
+++ w8/main.js
@@ -0,0 +1,36 @@
+import { renderTbl } from "./render.js";
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
+
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
+const cfpData = [];
+
+
+
+function start(first, last, HouseHoldMembers, houseSize) {
+    const houseHoldPTS = determineHouseHoldPts(HouseHoldMembers);
+    const houseSizePTS = determineHouseSizePts(houseSize);
+    const total = houseHoldPTS + houseSizePTS;
+    cfpData.push({
+        firstName: first,
+        lastName: last,
+        houseM: HouseHoldMembers,
+        houseS: houseSize,
+        houseMPTS: houseHoldPTS,
+        houseSPTS: houseSizePTS,
+        cfpTotal: total
+    });
+}
+
+FORM.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const firstName = FORM.firstname.value;
+    const lastName = FORM.lastname.value;
+    const houseMembers = parseInt(FORM.housemembers.value);
+    const houseSize = FORM.houses.value;
+    start(firstName, lastName, houseMembers, houseSize);
+    OUTPUT.innerHTML = "";
+    renderTbl(cfpData);
+    FORM.reset();
+})
+
---
+++ w8/render.js
@@ -0,0 +1,66 @@
+const TBL = document.getElementById("tab-data");
+
+function renderTblHeading() {
+    TBL.innerHTML = "";
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
+    headingTextArr.forEach(function (text) {
+        const th = document.createElement("th");
+        th.textContent = text;
+        tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    return table
+}
+
+function renderTblBtn(index, data) {
+    const td = document.createElement("td");
+    const btnEdit = document.createElement("button");
+    const btnDel = document.createElement("button");
+    btnEdit.textContent = "Edit";
+    btnDel.textContent = "Del";
+    td.appendChild(btnEdit);
+    td.appendChild(btnDel);
+    btnDel.addEventListener('click', function (e) {
+        console.log('Hello from the inside the delete button');
+        console.log(e);
+        data.splice(index, 1);
+        renderTbl(data);
+    })
+    btnEdit.addEventListener('click', function(e){
+        
+    })
+    return td;
+}
+
+function renderTblBody(data) {
+    const tbody = document.createElement("tbody")
+    data.forEach(function (obj, index) {
+        console.log(index);
+        const tr = document.createElement("tr");
+        for (const [key, value] of Object.entries(obj)) {
+            if (key !== "lastName" && key !== "houseMPTS" && key !== "houseSPTS") {
+                console.log("build td")
+                const td = document.createElement("td");
+                td.textContent = value;
+                tr.appendChild(td);
+            }
+        }
+        const td = renderTblBtn(index, data);
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+    });
+    return tbody;
+}
+
+function renderTbl(data) {
+    const table = renderTblHeading();
+    const tbody = renderTblBody(data)
+    table.appendChild(tbody);
+    TBL.appendChild(table);
+}
+
+export { renderTbl };
